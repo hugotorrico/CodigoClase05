7143cab91391b273cc32189a0d7877666025fa74
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2024-04-05 20:39:18 -0500

    Procedimiento IngresoNotas

--- CodigoClase05/Program.cs
+++ CodigoClase05/Program.cs
@@ -1,45 +1,86 @@
-﻿
-//Arreglos o Array
-int sumaNotas = 0;
-float promedio = 0;
-int cantidad = 10;
-int menor = 0;
-int mayor = 0;
-int[] notas = new int[cantidad];
-
-notas[0] = 20;
-notas[1] = 10;
-notas[2] = 10;
-notas[3] = 10;
-notas[4] = 10;
-notas[5] = 10;
-notas[6] = 10;
-notas[7] = 10;
-notas[8] = 10;
-notas[9] = 08;
-
-menor = notas[0];
-mayor = notas[0];
-
-//Identificar el menor
-for (int i = 0; i < notas.Length; i++)
+﻿int[] notas; 
+double CalcularPromedioForEach(int[] notas)
 {
-    //Calculando la nota menor
-    if (notas[i]<menor) 
-        menor = notas[i];
+    int sumaNotas = 0;
+    float promedio = 0;
+    int cantidad = notas.Length;
+    int menor = 0;
+    int mayor = 0;
 
-    //Calculando la nota mayor
-    if (notas[i] > mayor)
-        mayor = notas[i];
+    menor = notas[0];
+    mayor = notas[0];
+
+    foreach (var item in notas)
+    {
+        //Calculando la nota menor
+        if (item < menor)
+            menor = item;
+
+        //Calculando la nota mayor
+        if (item > mayor)
+            mayor = item;
+
+        //Sumando todo
+        sumaNotas = sumaNotas + item;
+    }
+
+    promedio = (sumaNotas - mayor - menor) / (cantidad - 2);
+    return promedio;
+}
+double CalcularPromedio(int[] notas)
+{
+    int sumaNotas = 0;
+    float promedio = 0;
+    int cantidad = notas.Length;
+    int menor = 0;
+    int mayor = 0;
+
+    menor = notas[0];
+    mayor = notas[0];
+
+    //Identificar el menor
+    for (int i = 0; i < cantidad; i++)
+    {
+        //Calculando la nota menor
+        if (notas[i] < menor)
+            menor = notas[i];
+
+        //Calculando la nota mayor
+        if (notas[i] > mayor)
+            mayor = notas[i];
+
+        //Sumando todo
+        sumaNotas = sumaNotas + notas[i];
+    }
+
+
+    promedio = (sumaNotas - mayor - menor) / (cantidad - 2);
+    return promedio;
+}
+void IngresarNotas()
+{
+    int cantidad = 0;
+    Console.WriteLine("INGRESE CANTIDAD DE NOTAS");
+    cantidad = Convert.ToInt32(Console.ReadLine());
+
+    //Arreglos 
+    notas = new int[cantidad]; //Usando For
     
-    //Sumando todo
-    sumaNotas = sumaNotas + notas[i];
+    for (int i = 0; i < cantidad; i++)
+    {
+        int numeroNota = i + 1;
+        Console.WriteLine("INGRESE NOTA[" + numeroNota + "]: ");
+        notas[i] = Convert.ToInt32(Console.ReadLine());
+    }
 }
 
-promedio = (sumaNotas-mayor-menor) / (cantidad-2);
 
-Console.WriteLine(menor);
-Console.WriteLine(mayor);
+//Encapsular(agrupar)
+double promedio = 0;
+
+IngresarNotas();
+promedio = CalcularPromedio(notas);
+
+Console.WriteLine("PROMEDIO ES: ");
 Console.WriteLine(promedio);
-
 Console.ReadLine(); // Para que el programa no se cierre inmediatamente
